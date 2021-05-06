fbca892d4eef6a1955e535c78545d88dc50e0145
Bill Ayers <BillA@flosim.com>
2021-05-06 14:36:37 +0100

    Changed imageSearch.js to final form

--- imageSearch.js
+++ imageSearch.js
@@ -7,7 +7,7 @@ function imageSearch(query) {
       var imageFile = categoryPictures[category][i];
       var name = imageName(imageFile);
       if (name.toLowerCase().indexOf(query.toLowerCase()) >= 0)
-        results.push(imageFile);
+        results.push(category + "/" + imageFile);
     }
   }
   return results;
@@ -15,10 +15,19 @@ function imageSearch(query) {
 
 function imageName(imageFile) {
   var name = imageFile;
+  if (imageFile.indexOf("/") >= 0) name = name.substr(name.indexOf("/")+1);
   if (name.indexOf("__") >= 0) name = name.substr(0, name.indexOf("__"));
   name = name.replace(/_/g, " ");
   return name;
 }
 
+function imageCategory(imageFile) {
+  var category = "";
+  if (imageFile.indexOf("/") >= 0) category = imageFile.substr(0, imageFile.indexOf("/"));
+  category = category.replace(/_/g, " ");
+  return category;
+}
+
 module.exports.imageSearch = imageSearch;
 module.exports.imageName = imageName;
+module.exports.imageCategory = imageCategory;
